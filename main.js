4322af598986e1f8215e0994a10593f6e03319c2
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-09-22 15:27:53 +0700

    finish

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const submit = document.getElementById("form");
-const subCard = document.getElementById("card-subscribe")
+const subCard = document.getElementById("card-subscribe");
+const disCard =document.getElementById("card-dismiss");
+const alert = document.getElementById("alert");
+const emailInfo = document.getElementById("guestEmail");
+const emailInput = document.getElementById("email");
+
+emailInput.addEventListener("click", (e)=>{
+    e.preventDefault();
+    emailInput.classList.remove("alert-style");
+    alert.classList.add("hidden");
+})
 
 submit.addEventListener("submit",(e) =>{
-    e.preventDefault()
+    e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
   
     if(!data.email.includes("@")){
-        console.log("email not corect!");
-    }
+        emailInput.classList.add("alert-style");
+        alert.classList.remove("hidden");
+    }else{
+        emailInfo.textContent = data.email;
+        subCard.classList.toggle("hidden");
+        disCard.classList.toggle("hidden");
+    };
 });
